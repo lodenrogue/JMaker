6e8411e5c1a6b46c857ab0776a2181021b4f4875
Miguel Hernandez <phyzix5761@gmail.com>
2016-03-19 09:11:34 -0400

    Initial Commit

---
+++ src/com/lodenrogue/jmaker/JMaker.java
@@ -0,0 +1,52 @@
+package com.lodenrogue.jmaker;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+public class JMaker {
+	private String eventName;
+	private String key;
+
+	public JMaker(String eventName, String key) {
+		this.eventName = eventName;
+		this.key = key;
+	}
+
+	public void trigger() throws IOException {
+		HttpClient httpClient = HttpClientBuilder.create().build();
+		HttpPost request = new HttpPost("https://maker.ifttt.com/trigger/" + eventName + "/with/key/" + key);
+		httpClient.execute(request);
+	}
+
+	public void trigger(List<String> values) throws IOException {
+		HttpClient httpClient = HttpClientBuilder.create().build();
+		HttpPost request = new HttpPost("https://maker.ifttt.com/trigger/" + eventName + "/with/key/" + key);
+		StringEntity params = new StringEntity(buildJson(values));
+		request.addHeader("content-type", "application/json");
+		request.setEntity(params);
+		httpClient.execute(request);
+	}
+
+	private String buildJson(List<String> values) {
+		String json = "{";
+		for (int i = 0; i < 3; i++) {
+			if (values.size() > i) {
+				json += "\"value" + (i + 1) + "\":\"" + values.get(i) + "\"";
+				if (values.size() != i + 1) {
+					json += ",";
+				}
+			}
+			else {
+				break;
+			}
+		}
+		json += "}";
+		return json;
+	}
+
+}
